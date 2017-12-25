2b4ce53e2f25419aa93a255b000676c08414a333
AKIHITO MORIMITSU <morimiwa4869@gmail.com>
2017-12-25 11:10:25 +0000

    Install the Bootstrap 3

---
+++ sample_app/config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+SampleApp::Application.config.secret_key_base = 'c0f0d42598f3e5e61f09714bf818effdecdb882a40c94165782d53aaec9a8b0344aa72df968cfac08202544c40612a497785e72e7e3db69a5e443fb1f5740dd1'
