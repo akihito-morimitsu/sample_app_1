147fd30c73e7f42163b5774ce1726473b5d54053
akihito-morimitsu <morimiwa4869@gmail.com>
2018-01-24 07:41:15 +0000

    Finish user follow / unfollow actions

--- sample_app/app/controllers/users_controller.rb
+++ sample_app/app/controllers/users_controller.rb
@@ -21,7 +21,21 @@ class UsersController < ApplicationController
     @microposts = @user.microposts.paginate(page: params[:page])
   end
   
-    private
+    def following
+    @title = "Following"
+    @user = User.find(params[:id])
+    @users = @user.followed_users.paginate(page: params[:page])
+    render 'users/show_follow'
+  end
+
+  def followers
+    @title = "Followers"
+    @user = User.find(params[:id])
+    @users = @user.followers.paginate(page: params[:page])
+    render 'users/show_follow'
+  end
+
+  private
     
     def admin_user
       redirect_to(root_path) unless current_user.admin?
--- sample_app/app/models/user.rb
+++ sample_app/app/models/user.rb
@@ -29,5 +29,4 @@ class User < ActiveRecord::Base
   def unfollow!(other_user)
     relationships.find_by(followed_id: other_user.id).destroy
   end
-
-
+end
